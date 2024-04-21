ca1e5283d92324f0316f6c59c07034ae268b934d
Danny Festor <danny.festor@gmail.com>
2024-04-21 14:27:29 +0900

    Install Alpinejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,13 @@
 import '../css/styles.css';
 
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+Alpine.start();
+
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('add your scripts');
+});
+
+document.addEventListener('alpine:init', () => {
+    console.log('Alpine Loaded');
 });
